1da37f789e6b9b974bf007ef49ee0f0b1334c388
Bruno Viana <francisco.lopes3@tjce.jus.br>
2023-12-11 14:49:36 -0300

    Consertando o gerador de tb

--- .github/workflows/gerarTabela.sh
+++ .github/workflows/gerarTabela.sh
@@ -3,8 +3,10 @@
 cd ../../00-Iniciante || exit
 
 echo "| ID | Nome | Link | Status |"
-for i in * ; do 
-    id="$(echo "$i" | cut -d "-" -f1)"; 
-    nome="$(echo "$i"| cut -d "-" -f2)"; 
-    echo "| $id | $nome | [$id](https://github.com/fbrunoviana/javascript-beecrowd/tree/main/00-Iniciante/$i) | ✅" 
+for i in * ; do
+    if [[ $i =~ ^[0-9]+- ]]; then
+        id="$(echo "$i" | cut -d "-" -f1)"
+        nome="$(echo "$i" | cut -d "-" -f2-)"
+        echo "| $id | $nome | [$id](https://github.com/fbrunoviana/javascript-beecrowd/tree/main/00-Iniciante/$i) | ✅ |"
+    fi
 done
